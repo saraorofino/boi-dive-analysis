fec57389aaf237bacd89a129f974d85361b25675
Sara Orofino <saraorofino@emLab-iMac-Pro.local>
2022-04-21 12:45:48 -0700

    Add ds store; add common R with filepaths

---
+++ common.R
@@ -0,0 +1,12 @@
+# A script to be sourced at the beginning of each working script to load working paths
+
+# Working paths
+sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/My Drive/",ifelse(Sys.info()["sysname"]=="Linux", "/home/jason/Documents/Gdrive_sync/emlab_shared/", "~/Google Drive/My Drive/"))
+# Path to this project's folder
+project_path <- paste0(sys_path,"emLab/Projects/boi-dive-project")
+#Path to project's data folder
+project_data_path <- file.path(project_path,"data")
+# Path to project's output table folder
+project_table_path <- file.path(project_path, "tables")
+# Path to project's output figure folder
+project_figure_path <- file.path(project_path, "figures")
