26d6480632db9931f5da13e062183207134cd6c7
Sara Orofino <saraorofino@bren.ucsb.edu>
2023-05-24 09:27:02 -0700

    Add plot and map themes

--- common.R
+++ common.R
@@ -1,7 +1,11 @@
 # A script to be sourced at the beginning of each working script to load working paths
 
+##########
+# File paths 
+##########
+
 # Working paths
-sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/My Drive/",ifelse(Sys.info()["sysname"]=="Linux", "/home/jason/Documents/Gdrive_sync/emlab_shared/", "~/Google Drive/My Drive/"))
+sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/My Drive/", "~/Google Drive/My Drive/")
 # Path to this project's folder
 project_path <- paste0(sys_path,"emLab/Projects/boi-dive-project")
 #Path to project's data folder
@@ -11,6 +15,35 @@ project_table_path <- file.path(project_path, "tables")
 # Path to project's output figure folder
 project_figure_path <- file.path(project_path, "figures")
 
-# Path to emLab data drive 
-emlab_data_dir <- file.path(ifelse(Sys.info()["sysname"]=="Windows", "G:/My Drive/",ifelse(Sys.info()["sysname"]=="Linux", "/home/jason/Documents/Gdrive_sync/emlab_shared/", "~/Google Drive/Shared drives/")),
-                            "emlab", "data")
+##########
+# Themes
+##########
+
+# Plot theme : matching specifications for Marine Policy figures 
+plot_theme <- function(base_family = "sans"){
+  
+  theme_bw(base_family = base_family) %+replace%
+    theme(strip.background = element_rect(fill = "white"),
+          panel.grid = element_blank(),
+          axis.title = element_text(size=8),
+          axis.text = element_text(size=8),
+          legend.title = element_text(size=8),
+          legend.text = element_text(size=8),
+          legend.position = "bottom",
+          text = element_text(family = 'sans'))
+}
+
+# Map theme: matching specifications for Marine Policy figures
+map_theme <- function(base_family = 'sans'){
+  
+  theme_bw(base_family = base_family) %+replace%
+    theme(legend.margin=margin(0,0,0,0, unit="cm"),
+          legend.position = 'bottom',
+          legend.direction = 'horizontal',
+          legend.title = element_text(size=7, family='sans'),
+          legend.text = element_text(size=7, family='sans'),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          text = element_text(family = "sans"))
+}
